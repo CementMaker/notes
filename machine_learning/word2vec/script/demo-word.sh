05b080bd1e4f70f397b158e28514897209c98922
zero91 <jianzhang9102@gmail.com>
2017-03-17 23:21:20 +0800

    add word2vec extensions paper

--- machine_learning/word2vec/script/demo-word.sh
+++ machine_learning/word2vec/script/demo-word.sh
@@ -6,16 +6,18 @@ if [ ! -e ${data_in}/text8 ]; then
     unzip ${data_in}/text8.gz -d ${data_in}
 fi
 
-time ../bin/word2vec -train ${data_in}/text8 \
-                     -output ${data_out}/vectors.txt \
-                     -cbow 1 \
-                     -size 200 \
-                     -window 8 \
-                     -negative 25 \
-                     -hs 1 \
-                     -sample 1e-4 \
-                     -threads 4 \
-                     -binary 0 \
-                     -iter 15
+if [ 1 -eq 1 ]; then
+    time ../bin/word2vec -train ${data_in}/text8 \
+                         -output ${data_out}/vectors.bin \
+                         -cbow 1 \
+                         -size 200 \
+                         -window 8 \
+                         -negative 25 \
+                         -hs 1 \
+                         -sample 1e-4 \
+                         -threads 4 \
+                         -binary 1 \
+                         -iter 15
+fi
 
-./distance ${data_out}/vectors.bin
+../bin/distance ${data_out}/vectors.bin
